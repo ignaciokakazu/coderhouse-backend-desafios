ab2b9e420e1fe34fde49bd732440e9602e2df17f
Ignacio Kakazu <ignaciokakazu@gmail.com>
2021-10-14 21:26:20 -0300

    desafio28-minimist

--- desafio28/dist/config/config.js
+++ desafio28/dist/config/config.js
@@ -5,9 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+var argv = require('minimist')(process.argv.slice(2));
+console.log(argv._);
+console.log(argv._[0].replace('FACEBOOK_APP_ID=', ''));
+console.log(argv._[1].replace('FACEBOOK_APP_SECRET=', ''));
 exports.default = {
     MONGO_ATLAS_URL: process.env.MONGO_ATLAS_SRV || 'mongoSRV',
     PORT: process.env.PORT || 8080,
-    FACEBOOK_APP_ID: process.argv[3] || process.env.FACEBOOK_APP_ID,
-    FACEBOOK_APP_SECRET: process.argv[4] || process.env.FACEBOOK_APP_SECRET,
+    FACEBOOK_APP_ID: argv._[0].replace('FACEBOOK_APP_ID=', '') || process.env.FACEBOOK_APP_ID,
+    FACEBOOK_APP_SECRET: argv._[1].replace('FACEBOOK_APP_SECRET=', '') || process.env.FACEBOOK_APP_SECRET,
 };
