ea0d97b8155781a12e13c071da9d1c4230b68309
Ignacio Kakazu <ignaciokakazu@gmail.com>
2021-10-28 22:36:52 -0300

    desafio32 a modificar

---
+++ desafio32/ecosystem.config.js
@@ -0,0 +1,31 @@
+/**
+ * https://pm2.keymetrics.io/docs/usage/application-declaration/
+ */
+
+//para ejecutar: pm2 start ecosystem.config.js
+//pm2 list
+//desde 1:40
+ module.exports = {
+  apps: [
+    {
+      name: 'app1',
+      script: 'dist/app.js',
+      watch: true,
+      autorestart: true, //por si se cae, reinicia
+      // instances: 4,
+      args: '--puerto=8081', //esto funciona en combinación con argv y config.PORT. VER nginx.config
+    },
+    {
+      name: 'app2',
+      script: 'dist/app.js',
+      watch: true,
+      autorestart: true, //por si se cae, reinicia
+      // instances: 4,
+      args: '--puerto=8082', //esto funciona en combinación con argv y config.PORT. VER nginx.config
+    },
+    {
+      script: './service-worker/',
+      watch: ['./service-worker'],
+    },
+  ],
+};
