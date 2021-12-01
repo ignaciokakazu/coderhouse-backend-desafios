c578c23e35de8c99d638f3261e611ca8a2aa7c39
Ignacio Kakazu <ignaciokakazu@gmail.com>
2021-11-30 23:35:28 -0300

    desafio41 incompleto

---
+++ desafio41/src/app.ts
@@ -0,0 +1,47 @@
+import myServer from './services/server';
+import config from './config/config';
+import {infoLogger, peligroLogger} from './services/logger';
+import os from 'os';
+import cluster from 'cluster';
+import minimist from 'minimist';
+
+const PORT = process.env.PORT || config.PORT;
+
+/* FORK O CLUSTER */
+const argv = minimist(process.argv.slice(2));
+const modo = argv.server || 'FORK';
+export const persistencia = argv.persistencia || "MOA";
+
+/* POR MOTIVOS DE TESTING CON SUPERTEST, SAQUÉ EL LISTEN DE ACÁ 
+
+ https://zellwk.com/blog/endpoint-testing/ */
+
+if (modo === 'FORK') {
+
+    myServer.listen(PORT, () => infoLogger.info(`Server Up port modo fork ${PORT}`));
+
+    infoLogger.info(process.pid);
+    process.on('exit', (code)=> {
+        infoLogger.info(`Código de salida: ${code}`);
+    })
+
+} else {
+
+    if (cluster.isMaster && cluster.isPrimary) {
+        const numCPU = os.cpus().length
+        infoLogger.info(`NÚMERO DE CPUS => ${numCPU}`);
+        infoLogger.info(`PID MASTER => ${process.pid}`);
+
+        for (let i=0;i<numCPU;i++) {
+            cluster.fork();
+        }
+
+        cluster.on('exit', (worker)=> {
+            infoLogger.info(`Worker ${worker.process.pid} died at ${new Date()}`);
+            cluster.fork();
+        })
+    }
+
+        myServer.listen(PORT, () => infoLogger.info(`Server Up port modo cluster ${PORT} - PID WORKER ${process.pid}`));
+    
+}
